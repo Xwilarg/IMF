e388d52998fdaf6dc28c5c5c2d66f546d4c6d619
Xwilarg <xwilarg@yahoo.fr>
2020-06-03 17:08:19 +0200

    Add base project

---
+++ WindowsFormsApp2/Form1.Designer.cs
@@ -0,0 +1,95 @@
+﻿namespace WindowsFormsApp2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.cartesianChart1 = new LiveCharts.WinForms.CartesianChart();
+            this.button1 = new System.Windows.Forms.Button();
+            this.cartesianChart2 = new LiveCharts.WinForms.CartesianChart();
+            this.cartesianChart3 = new LiveCharts.WinForms.CartesianChart();
+            this.SuspendLayout();
+            // 
+            // cartesianChart1
+            // 
+            this.cartesianChart1.Location = new System.Drawing.Point(12, 12);
+            this.cartesianChart1.Name = "cartesianChart1";
+            this.cartesianChart1.Size = new System.Drawing.Size(776, 193);
+            this.cartesianChart1.TabIndex = 0;
+            this.cartesianChart1.Text = "cartesianChart1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(362, 211);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Clean";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // cartesianChart2
+            // 
+            this.cartesianChart2.Location = new System.Drawing.Point(12, 240);
+            this.cartesianChart2.Name = "cartesianChart2";
+            this.cartesianChart2.Size = new System.Drawing.Size(776, 198);
+            this.cartesianChart2.TabIndex = 2;
+            this.cartesianChart2.Text = "cartesianChart2";
+            // 
+            // cartesianChart3
+            // 
+            this.cartesianChart3.Location = new System.Drawing.Point(12, 457);
+            this.cartesianChart3.Name = "cartesianChart3";
+            this.cartesianChart3.Size = new System.Drawing.Size(773, 225);
+            this.cartesianChart3.TabIndex = 3;
+            this.cartesianChart3.Text = "cartesianChart3";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(797, 706);
+            this.Controls.Add(this.cartesianChart3);
+            this.Controls.Add(this.cartesianChart2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.cartesianChart1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private LiveCharts.WinForms.CartesianChart cartesianChart1;
+        private System.Windows.Forms.Button button1;
+        private LiveCharts.WinForms.CartesianChart cartesianChart2;
+        private LiveCharts.WinForms.CartesianChart cartesianChart3;
+    }
+}
+
---
+++ WindowsFormsApp2/Form1.cs
@@ -0,0 +1,160 @@
+﻿using LiveCharts;
+using LiveCharts.Wpf;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp2
+{
+    public partial class Form1 : Form
+    {
+        public static readonly float MIDDLE_LIMIT = .5f;
+        public static readonly float RANGE_MIN = -100f;
+        public static readonly float RANGE_MAX = 100f;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            var rand = new Random();
+            ChartValues<float> content = new ChartValues<float>();
+            for (int i = 0; i < 30; i++)
+            {
+                double sample = rand.NextDouble();
+                double scaled = (sample * (RANGE_MAX + Math.Abs(RANGE_MIN))) + RANGE_MIN;
+                content.Add((float)scaled);
+            }
+            cartesianChart1.Series = new SeriesCollection
+            {
+                new LineSeries
+                {
+                    Values = content
+                }
+            };
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            var allValues = cartesianChart1.Series[0].Values.Cast<float>().ToArray();
+            if (allValues.Length < 2)
+                return;
+            List<float> contentMiddle;
+            int debugCounter = 0;
+            do
+            {
+                contentMiddle = new List<float>();
+                List<float> contentUp = new List<float>();
+                List<float> contentDown = new List<float>();
+                float first = allValues[0];
+                float last = first;
+                bool nextIsUp = allValues[1] > last;
+                int i = 1;
+                foreach (int val in allValues.Skip(1))
+                {
+                    if (nextIsUp)
+                    {
+                        if (val < last)
+                        {
+                            contentUp.Add(last);
+                            nextIsUp = false;
+                        }
+                        else
+                        {
+                            contentUp.Add(contentUp.Count > 0 ? contentUp.Last() : 0f);
+                        }
+                        contentDown.Add(contentDown.Count > 0 ? contentDown.Last() : 0f);
+                    }
+                    else
+                    {
+                        if (val > last)
+                        {
+                            contentDown.Add(last);
+                            nextIsUp = true;
+                        }
+                        else
+                        {
+                            contentDown.Add(contentDown.Count > 0 ? contentDown.Last() : 0f);
+                        }
+                        contentUp.Add(contentUp.Count > 0 ? contentUp.Last() : 0f);
+                    }
+                    last = val;
+                    i++;
+                }
+                var lastValue = allValues.Last();
+                var beforeLastValue = allValues[allValues.Length - 2];
+                if (nextIsUp)
+                {
+                    if (beforeLastValue < lastValue)
+                    {
+                        contentUp.Add(last);
+                    }
+                    else
+                    {
+                        contentUp.Add(contentUp.Count > 0 ? contentUp.Last() : 0f);
+                    }
+                    contentDown.Add(contentDown.Count > 0 ? contentDown.Last() : 0f);
+                }
+                else
+                {
+                    if (beforeLastValue > lastValue)
+                    {
+                        contentDown.Add(last);
+                    }
+                    else
+                    {
+                        contentDown.Add(contentDown.Count > 0 ? contentDown.Last() : 0f);
+                    }
+                    contentUp.Add(contentUp.Count > 0 ? contentUp.Last() : 0f);
+                }
+                for (int y = 0; y < contentUp.Count; y++)
+                    contentMiddle.Add((contentUp[y] + contentDown[y]) / 2f);
+                cartesianChart2.Series = new SeriesCollection
+                    {
+                        new LineSeries
+                        {
+                            Values = new ChartValues<float>(contentUp)
+                        },
+                        new LineSeries
+                        {
+                            Values = new ChartValues<float>(contentMiddle)
+                        },
+                        new LineSeries
+                        {
+                            Values = new ChartValues<float>(contentDown)
+                        },
+                        new LineSeries
+                        {
+                            Values = new ChartValues<float>(allValues)
+                        }
+                    };
+                List<float> imf = new List<float>();
+                List<float> residue = new List<float>();
+                for (int y = 0; y < contentMiddle.Count; y++)
+                {
+                    var tmp = allValues[y] - contentMiddle[y];
+                    imf.Add(tmp);
+                    residue.Add(allValues[y] - tmp);
+                }
+                cartesianChart3.Series = new SeriesCollection
+                    {
+                        new LineSeries
+                        {
+                            Values = new ChartValues<float>(imf)
+                        },
+                        new LineSeries
+                        {
+                            Values = new ChartValues<float>(residue)
+                        }
+                    };
+                allValues = residue.ToArray();
+                MessageBox.Show(this, "WAIT");
+                debugCounter++;
+            } while (contentMiddle.Any(v => Math.Abs(v) > MIDDLE_LIMIT)); // 
+            MessageBox.Show(this, "Done");
+        }
+    }
+}
